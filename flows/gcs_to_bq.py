f857ee94b6905a7d08a65b6bb0a90cf9e010f256
Cyrus <cyrus@Cyruss-Mac-mini.local>
2023-05-08 19:04:55 +0100

    Added gcs to bq flow

--- flows/gcs_to_bq.py
+++ flows/gcs_to_bq.py
@@ -1,1 +1,23 @@
+from prefect_gcp.bigquery import bigquery_load_cloud_storage
+from prefect import flow, task
+from prefect_gcp import GcpCredentials
 
+@flow(name="Divvy trips GCS to BQ")
+def gcs_to_bq():
+    gcp_credentials_block = GcpCredentials.load("divvy-trips-creds")
+
+    result = bigquery_load_cloud_storage(
+        dataset="raw",
+        table="test_table",
+        uri="gs://la_311/*.parquet",
+        location="europe-west2",
+        gcp_credentials=gcp_credentials_block,
+        job_config={
+            "source_format": 'PARQUET'
+        }
+    )
+
+    return result
+
+if __name__ == "__main__":
+    gcs_to_bq()
